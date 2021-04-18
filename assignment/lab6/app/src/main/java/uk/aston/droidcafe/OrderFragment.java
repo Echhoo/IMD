071516395a6269e2739d7d938f9495d28b91ee08
Echhoo <1799956902@qq.com>
2021-04-19 03:44:41 +0800

    add lab6 work

---
+++ assignment/lab6/app/src/main/java/uk/aston/droidcafe/OrderFragment.java
@@ -0,0 +1,46 @@
+package uk.aston.droidcafe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class OrderFragment extends AppCompatActivity {
+    private String mOrderMessage;
+    public static final String EXTRA_MESSAGE =
+            "us.aston.android.droidcafe.extra.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_order);
+        Intent intent = getIntent();
+        String message = "Order: " +
+                intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.order_textview);
+        textView.setText(message);
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    public void showDonutOrder(View view) {
+        mOrderMessage = getString(R.string.donut_order_message);
+        displayToast(mOrderMessage);
+    }
+
+    public void showIceCreamOrder(View view) {
+        mOrderMessage = getString(R.string.ice_cream_order_message);
+        displayToast(mOrderMessage);
+    }
+
+    public void showFroyoOrder(View view) {
+        mOrderMessage = getString(R.string.froyo_order_message);
+        displayToast(mOrderMessage);
+    }
+}
